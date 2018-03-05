3490dc01a978fb28ccfac65eb59a7a8c31008333
JNelson <nelson.jimmy@gmail.com>
2018-03-05 08:53:45 -0700

    added link to about me page.

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -4,10 +4,31 @@ require 'pry'
 # Defines Personal Site
 class PersonalSite
   def self.call(env)
+    case env['PATH_INFO']
+    when '/' then index
+    when '/about' then about
+    else
+      error
+    end
+  end
+
+  def self.index
+    render_view('index.html')
+  end
+
+  def self.about
+    render_view('about.html')
+  end
+
+  def self.error
+    render_view('error.html', '404')
+  end
+
+  def self.render_view(page, code = '200')
     [
-      '200',
+      code,
       { 'Content-Type' => 'text/html' },
-      [File.read('./app/views/index.html')]
+      [File.read("./app/views/#{page}")]
     ]
   end
 end
--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -8,4 +8,18 @@ class HomepageTest < CapybaraTestCase
     assert page.has_content?('Welcome!')
     assert_equal 200, page.status_code
   end
+
+  def test_raises_an_error_if_page_doesnt_exist
+    visit '/blog'
+
+    assert page.has_content?('Page not found.')
+    assert_equal 404, page.status_code
+  end
+
+  def test_about_me_has_info_about_me
+    visit '/about'
+
+    assert page.has_content?("Here's some stuff to know.")
+    assert_equal 200, page.status_code
+  end
 end
