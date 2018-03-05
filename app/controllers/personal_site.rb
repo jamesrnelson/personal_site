9689f3f7542ad8a950e042b6b3a8e157d649a45f
JNelson <nelson.jimmy@gmail.com>
2018-03-05 08:07:56 -0700

    added call method to personal site. Pried into the class and the test to see what was going on.

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -1,3 +1,9 @@
-class PersonalSite
+require 'rack'
+require 'pry'
 
+# Defines Personal Site
+class PersonalSite
+  def self.call(env)
+    ['200', { 'Content-Type' => 'text/html' }, ['Welcome!']]
+  end
 end
--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -1,9 +1,11 @@
 require './test/test_helper'
+require 'pry'
 
 class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
 
+    binding.pry
     assert page.has_content?('Welcome!')
     assert_equal 200, page.status_code
   end
